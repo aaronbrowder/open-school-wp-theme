9069f94d0ac9d3a2ce5d9293241d33ac53927cbd
Aaron Browder <aaronbrowder@gmail.com>
2017-02-08 06:55:20 +0000

    Add donate button to blog

--- wp-content/themes/theopenschool/content-single.php
+++ wp-content/themes/theopenschool/content-single.php
@@ -14,29 +14,45 @@
 <hr/>
 <br/>
 
-<h4>Share this</h4>
-
-<?php
-$url = urlencode(get_permalink());
-$title = urlencode(get_the_title() . ' – ' . get_bloginfo('name'));
-$email_body = urlencode("Hi,\n\nI thought you'd like this:\n" . get_permalink());
-?>
-
-<a class="blog-share-facebook" 
-   href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $url; ?>"
-   target="_blank">
-  <span class="icon icon-facebook-square"></span>
-</a>
-
-<a class="blog-share-email"
-   href="mailto:?subject=<?php echo $title; ?>&body=<?php echo $email_body; ?>"
-   target="_blank">
-   <span class="icon icon-envelope"></span>
-</a>
+<div class="blog-footer">
+   
+   <div class="blog-share">
+      <h4>Share this</h4>
+      <?php
+      $url = urlencode(get_permalink());
+      $title = urlencode(get_the_title() . ' – ' . get_bloginfo('name'));
+      $email_body = urlencode("Hi,\n\nI thought you'd like this:\n" . get_permalink());
+      ?>
+      <a class="blog-share-facebook" 
+         href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $url; ?>"
+         target="_blank">
+        <span class="icon icon-facebook-square"></span>
+      </a>
+      <a class="blog-share-email"
+         href="mailto:?subject=<?php echo $title; ?>&body=<?php echo $email_body; ?>"
+         target="_blank">
+         <span class="icon icon-envelope"></span>
+      </a>
+   </div>
+   
+   <div class="blog-donate">
+      <h4>Support our school</h4>
+      <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+         <input name="cmd" type="hidden" value="_s-xclick" />
+         <input name="hosted_button_id" type="hidden" value="6UHSFSZE64B4C" />
+         <button type="submit" name="submit" class="blog-donate-button">Donate</button>
+      </form>
+   </div>
+      
+</div>
 
 <nav class="blog-paginator">
 	<ul>
-		<li><?php next_post_link('%link', '&laquo;&nbsp; %title'); ?></li>
+	   <?php
+	   $next_post_link = get_next_post_link('%link', '&laquo;&nbsp; %title');
+	   if (!empty($next_post_link)) { ?>
+	      <li><?php echo $next_post_link ?></li>
+	   <?php } ?>
 		<li><?php previous_post_link('%link', '%title &nbsp;&raquo;'); ?></li>
 	</ul>
 </nav>
