0b591bbb9ee5fe00526c18efc7cd30618d347ec2
Aaron Browder <aaronb@tactics.com>
2020-04-07 15:26:53 -0700

    Enable comments

--- wp-content/themes/theopenschool/single.php
+++ wp-content/themes/theopenschool/single.php
@@ -8,9 +8,9 @@
       
          get_template_part('content-single', get_post_format());
         
-      //    if ( comments_open() || get_comments_number() ) :
-      // 	   comments_template();
-      // 	endif;
+         if ( comments_open() || get_comments_number() ) :
+      	   comments_template();
+      	endif;
       
       endwhile; endif;
       ?>
