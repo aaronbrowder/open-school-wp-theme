ef83ce61576ab0b8287c75df7aca48b59dc7ccff
Aaron Browder <aaronbrowder@gmail.com>
2022-08-16 18:35:25 -0700

    Add book promo to the end of blog posts

--- wp-content/themes/theopenschool/content-single.php
+++ wp-content/themes/theopenschool/content-single.php
@@ -29,42 +29,52 @@ if (sizeof($pieces) > 0 && $pieces[0] == 'product') {
 <?php if (!$is_product_page) { ?>
    <div class="blog-footer">
       
-      <div class="blog-share">
-         <h4>Share this</h4>
-         <?php
-         $url = urlencode(get_permalink());
-         $title = get_the_title();
-         $span_str = '<span class="entry-title-primary">';
-         if (startsWith($title, $span_str)) {
-            $span_length = strlen($span_str);
-            $title = substr($title, $span_length);
-            $end_span_pos = strpos($title, '</span>');
-            $title = substr($title, 0, $end_span_pos);
-         }
-         $subject = urlencode($title . ' – ' . get_bloginfo('name'));
-         $email_body = urlencode("Hi,\n\nI thought you'd like this:\n" . get_permalink());
-         ?>
-         <a class="blog-share-facebook" 
-            href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $url; ?>"
-            target="_blank">
-           <i class="fab fa-facebook-square"></i>
-         </a>
-         <a class="blog-share-twitter" 
-            href="https://twitter.com/share"
-            target="_blank">
-           <i class="fab fa-twitter"></i>
-         </a>
-         <a class="blog-share-email"
-            href="mailto:?subject=<?php echo $subject; ?>&body=<?php echo $email_body; ?>"
-            target="_blank">
-            <i class="fas fa-envelope"></i>
-         </a>
+      <div class="blog-book-promo">
+         <div class="blog-book-promo-image">
+            <a href="https://www.openschooloc.com/empowered-kids-liberated-parents/">
+               <img src="https://www.openschooloc.com/wp-content/uploads/2022/08/blog-footer-book-promo.png">
+            </a>
+         </div>
+         <div class="blog-book-promo-text">
+            Like this blog post? Why not check out the book we wrote:
+            <a href="https://www.openschooloc.com/empowered-kids-liberated-parents/">Empowered Kids, Liberated Parents</a>
+         </div>
       </div>
+      <div class="blog-promo-divider"></div>
       
       <div class="blog-buttons">
-         <div class="blog-apply">
+         <div class="blog-other-promo">
             <h4>Join our school</h4>
             <a href="/wp/admissions" class="small-button">Apply Now</a>
+            <h4>Share this</h4>
+            <?php
+            $url = urlencode(get_permalink());
+            $title = get_the_title();
+            $span_str = '<span class="entry-title-primary">';
+            if (startsWith($title, $span_str)) {
+               $span_length = strlen($span_str);
+               $title = substr($title, $span_length);
+               $end_span_pos = strpos($title, '</span>');
+               $title = substr($title, 0, $end_span_pos);
+            }
+            $subject = urlencode($title . ' – ' . get_bloginfo('name'));
+            $email_body = urlencode("Hi,\n\nI thought you'd like this:\n" . get_permalink());
+            ?>
+            <a class="blog-share-facebook" 
+               href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $url; ?>"
+               target="_blank">
+              <i class="fab fa-facebook-square"></i>
+            </a>
+            <a class="blog-share-twitter" 
+               href="https://twitter.com/share"
+               target="_blank">
+              <i class="fab fa-twitter"></i>
+            </a>
+            <a class="blog-share-email"
+               href="mailto:?subject=<?php echo $subject; ?>&body=<?php echo $email_body; ?>"
+               target="_blank">
+               <i class="fas fa-envelope"></i>
+            </a>
          </div>
       </div>
       
